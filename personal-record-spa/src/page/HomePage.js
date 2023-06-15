b0a1ee9cecd391284343dcc822dc15bcf1e3c57a
hyojinLeee-git <splato8899@gmail.com>
2023-06-15 23:25:17 +0900

    feat: add homepage, signup page

---
+++ personal-record-spa/src/page/HomePage.js
@@ -0,0 +1,11 @@
+class HomePage {
+    constructor($main) {
+        this.$main = $main;
+    }
+
+    render() {
+        console.log("home");
+    }
+}
+
+export default HomePage;
---
+++ personal-record-spa/src/page/SignupPage.js
@@ -0,0 +1,11 @@
+class SignupPage {
+    constructor($main) {
+        this.$main = $main;
+    }
+
+    render() {
+        console.log("sign up");
+    }
+}
+
+export default SignupPage;
