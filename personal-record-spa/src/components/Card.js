ecd0ea55a17086c76c55146139821ffaa64a46bb
hyojinLeee-git <splato8899@gmail.com>
2023-07-02 20:48:03 +0900

    feat:add flip animation

--- personal-record-spa/src/components/Card.js
+++ personal-record-spa/src/components/Card.js
@@ -3,13 +3,18 @@ export const cardDiv = (idx) => {
     _cardDiv.setAttribute("idx", idx);
     _cardDiv.setAttribute("class", "card");
 
+    _cardDiv.addEventListener("click", () => {
+        _cardDiv.classList.toggle("is-flipped");
+    });
+
     return _cardDiv;
 };
 
 export const cardPlane = (side, data) => {
     const _cardPlane = document.createElement("div");
     _cardPlane.setAttribute("class", `card_plane card_plane--${side}`);
-    _cardPlane.innerHTML = data.name;
+    if (side === "front") _cardPlane.innerHTML = data.name;
+    else _cardPlane.innerHTML = data.mbti;
 
     return _cardPlane;
 };
