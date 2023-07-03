b81e8d43be44bc47b44f2bbe9737a5cdf7071c20
hyojinLeee-git <splato8899@gmail.com>
2023-07-03 22:27:59 +0900

    feat:add signup view

---
+++ personal-record-spa/src/components/SignupView.js
@@ -0,0 +1,13 @@
+class SignupView {
+    constructor($main) {
+        this.$main = $main;
+    }
+
+    render() {
+        const containerDiv = document.createElement("div");
+        containerDiv.setAttribute("id", "form_container");
+        this.$main.appendChild(containerDiv);
+    }
+}
+
+export default SignupView;
--- personal-record-spa/src/page/SignupPage.js
+++ personal-record-spa/src/page/SignupPage.js
@@ -1,10 +1,16 @@
+import ContentTitle from "../components/ContentTitle.js";
+import SignupView from "../components/SignupView.js";
+
 class SignupPage {
     constructor($main) {
         this.$main = $main;
     }
 
     render() {
-        console.log("sign up");
+        const $title = new ContentTitle(this.$main, "Sign Up, GreatPeoPle!");
+        const $signupView = new SignupView(this.$main);
+        $title.render();
+        $signupView.render();
     }
 }
 
