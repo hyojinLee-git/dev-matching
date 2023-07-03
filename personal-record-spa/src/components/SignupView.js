23cd33d56839bed9e8a808a6f991abf1ecf9b416
hyojinLeee-git <splato8899@gmail.com>
2023-07-03 22:29:20 +0900

    feat:add form tag

--- personal-record-spa/src/components/SignupView.js
+++ personal-record-spa/src/components/SignupView.js
@@ -7,6 +7,10 @@ class SignupView {
         const containerDiv = document.createElement("div");
         containerDiv.setAttribute("id", "form_container");
         this.$main.appendChild(containerDiv);
+
+        const form = document.createElement("form");
+        form.setAttribute("id", "grepp_form");
+        containerDiv.appendChild(form);
     }
 }
 
