aaf0ca2e7f973ad958a29003ca7362586476884b
hyojinLeee-git <splato8899@gmail.com>
2023-07-03 23:00:00 +0900

    feat: add input field

--- personal-record-spa/src/components/SignupView.js
+++ personal-record-spa/src/components/SignupView.js
@@ -1,3 +1,5 @@
+import { button, input, select } from "./Form.js";
+
 class SignupView {
     constructor($main) {
         this.$main = $main;
@@ -11,6 +13,66 @@ class SignupView {
         const form = document.createElement("form");
         form.setAttribute("id", "grepp_form");
         containerDiv.appendChild(form);
+
+        input("text", "name", "이름");
+        input("email", "email", "이메일");
+        input("text", "nickname", "닉네임");
+
+        const roleValList = [
+            "직군을 선택해주세요",
+            "프론트엔드",
+            "백엔드",
+            "풀스택",
+        ];
+        const roleTxtList = [
+            "직군을 선택해주세요",
+            "프론트엔드",
+            "백엔드",
+            "풀스택",
+        ];
+        const mbtiValList = [
+            "MBTI를 선택해주세요",
+            "ENFJ",
+            "ENTJ",
+            "ENFP",
+            "ENTP",
+            "ESFJ",
+            "ESTJ",
+            "ESFP",
+            "ESTP",
+            "INFJ",
+            "INTJ",
+            "INFP",
+            "INTP",
+            "ISFJ",
+            "ISTJ",
+            "ISFP",
+            "ISTP",
+        ];
+        const mbtiTxtList = [
+            "MBTI를 선택해주세요",
+            "ENFJ",
+            "ENTJ",
+            "ENFP",
+            "ENTP",
+            "ESFJ",
+            "ESTJ",
+            "ESFP",
+            "ESTP",
+            "INFJ",
+            "INTJ",
+            "INFP",
+            "INTP",
+            "ISFJ",
+            "ISTJ",
+            "ISFP",
+            "ISTP",
+        ];
+
+        select("role", roleValList, roleTxtList);
+        select("mbti", mbtiValList, mbtiTxtList);
+
+        button("submit", "등록");
     }
 }
 
